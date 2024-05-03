681e399af4a563d1f724e4c70c63b2ff7e004303
半代大和 <y.handai1272@gmail.com>
2024-05-03 12:34:13 +0900

    feat(menu): Adjuted menu layout

--- components/header.tsx
+++ components/header.tsx
@@ -6,14 +6,14 @@ export default function Header() {
   return (
     <header className="border-b">
       <div className="mx-auto flex h-[var(--nav-height)] max-w-6xl items-center justify-between px-5">
-        <Link href="/" className="group flex items-center gap-1">
-          <span className="font-ita font-sans text-lg font-semibold tracking-tight">
-            Dev<span className="inline-block w-1"></span>Toolbox
+        <Link
+          href="/"
+          className="group flex items-center gap-0.5 text-foreground/90 duration-300 hover:text-foreground"
+        >
+          <span className="font-sans text-lg font-semibold tracking-tight">
+            Dev<span className="inline-block w-0.5"></span>Toolbox
           </span>
-          <Logo
-            className="rotate-[8deg] duration-300 group-hover:rotate-[4deg]"
-            aria-hidden="true"
-          />
+          <Logo className="rotate-[8deg]" aria-hidden="true" />
         </Link>
         <Menu />
       </div>
--- components/side-menu.tsx
+++ components/side-menu.tsx
@@ -2,7 +2,7 @@ import CategoryList from './category-list'
 
 export default function SideMenu() {
   return (
-    <aside className="z-0 min-h-[100svh] w-[200px] border-r px-3 pt-8">
+    <aside className="fiexd left-[-100%] z-0 min-h-[100svh] min-w-[200px] border-r px-3 pt-8">
       <nav aria-label="サイドメニュー">
         <div className="hover:[&>a]:durataion-300">
           <CategoryList />
