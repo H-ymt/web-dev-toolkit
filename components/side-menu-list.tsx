aad049c0aa050b4b28f3dced0062f9b64f3d88f2
半代大和 <y.handai1272@gmail.com>
2024-05-09 21:50:49 +0900

    feat(sidemenu): Update side menu

--- components/side-menu-list.tsx
+++ components/side-menu-list.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link'
 import CategoryList from './category-list'
 
 export default function SideMenuList() {
   return (
     <aside className="fiexd left-[-100%] z-0 h-screen min-h-[100vh] min-w-[270px] overflow-scroll overflow-x-hidden px-8 pb-24 pt-8 lg:border-r">
-      <p className="pb-3 text-left">Categories</p>
+      <div className="grid justify-items-start gap-3 pb-3">
+        <Link href="/">All</Link>
+        <p>Categories</p>
+      </div>
       <nav aria-label="サイドメニュー">
         <div className="hover:[&>a]:durataion-300">
           <CategoryList />
